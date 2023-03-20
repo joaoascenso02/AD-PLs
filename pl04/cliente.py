cb556af6b045a158006eb846377ccd3273b58492
Joao Ascenso <joao.tp.ascenso@gmail.com>
2023-03-20 15:01:16 +0000

    pl04 select

---
+++ pl04/cliente.py
@@ -0,0 +1,81 @@
+import pickle as p
+import struct
+from argparse import ArgumentParser
+
+import sock_utils
+
+
+def parse() -> dict:
+    parser = ArgumentParser(
+        description="Cliente"
+    )
+
+    parser.add_argument(
+        "address",
+        help="ip ou hostname do servidor",
+        default="localhost"
+    )
+
+    parser.add_argument(
+        "port",
+        help="porto tcp onde o servidor recebe pedidos de ligacao",
+        type=int,
+        default=9999
+    )
+
+    args = parser.parse_args().__dict__
+
+    return args
+
+
+def main() -> None:
+
+    args = parse()
+
+    try:
+
+        HOST = args["address"]
+        PORT = args["port"]
+
+        sock = sock_utils.create_tcp_client_socket(HOST, PORT)
+
+        while True:
+            try:
+                msg = list()
+                msg.append(str(input("Mensagem: ")))
+
+                if msg == [""]:
+                    continue
+                elif msg == ["EXIT"]:
+                    break
+
+                msg_bytes = p.dumps(msg)
+                msg_size_bytes = struct.pack("i", len(msg_bytes))
+
+                sock.sendall(msg_size_bytes)
+                sock.sendall(msg_bytes)
+
+                resp_size_bytes = sock_utils.receive_all(sock, 4)
+                resp_size = struct.unpack("i", resp_size_bytes)[0]
+
+                resp_bytes = sock_utils.receive_all(sock, resp_size)
+                m = p.loads(resp_bytes)
+
+                print("Recebi: %s" % m)
+
+            except Exception as e:
+                print(e)
+                break
+
+        sock.close()
+
+    except KeyboardInterrupt:
+        print("\n Vou encerrar!")
+        pass
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pl04/servidor.py
@@ -0,0 +1,116 @@
+import pickle as p
+import struct
+import select as sel
+import sys
+from argparse import ArgumentParser
+
+import sock_utils
+
+
+def parse() -> dict:
+    parser = ArgumentParser(
+        description="Servidor"
+    )
+
+    parser.add_argument(
+        "address",
+        help="ip ou hostname do servidor",
+        default="localhost"
+    )
+
+    parser.add_argument(
+        "port",
+        help="porto tcp onde o servidor recebe pedidos de ligacao",
+        type=int,
+        default=9999
+    )
+
+    args = parser.parse_args().__dict__
+
+    return args
+
+
+def main() -> None:
+
+    args = parse()
+
+    try:
+
+        HOST = args["address"]
+        PORT = args["port"]
+
+        listen_socket = sock_utils.create_tcp_server_socket(HOST, PORT, 1)
+
+        socket_list = [listen_socket, sys.stdin]
+
+        lista = []
+        while True:
+            try:
+                
+                R, W, X = sel.select(socket_list, [], [])
+                
+                for sckt in R:
+                    if sckt is sys.stdin:
+                        msg = sys.stdin.readline().strip()
+                        if msg == "EXIT":
+                            raise SystemExit()
+
+                    # se for a socket de escuta
+                    if sckt is listen_socket:
+                        conn_sock, addr = listen_socket.accept()
+                        addr, port = conn_sock.getpeername()
+                        print("Novo cliente ligado desde %s:%d" %(addr, port))
+                        # adiciona ligacao a lista
+                        socket_list.append(conn_sock)
+
+                    # se for a socket de um cliente
+                    else:
+                        req_size_bytes = sock_utils.receive_all(sckt, 4)
+                        req_size = struct.unpack("i", req_size_bytes)[0]
+
+                        req_bytes = sock_utils.receive_all(sckt, req_size)
+                        req = p.loads(req_bytes)
+
+                        if req:
+
+                            resp = ["Ack"]
+
+                            if req == ["LIST"]:
+                                resp = lista
+
+                            elif req == ["CLEAR"]:
+                                lista = []
+                                resp = ["Lista apagada"]
+
+                            else:
+                                lista.extend(req)
+
+                            resp_bytes = p.dumps(resp)
+                            resp_size_bytes = struct.pack("i", len(resp_bytes))
+
+                            sckt.sendall(resp_size_bytes)
+                            sckt.sendall(resp_bytes)
+
+                            print("lista = %s" % lista)
+                        
+                        else:
+                            sckt.close()
+                            socket_list.remove(sckt)
+                            print("Cliente fechou a ligacao")
+
+            except Exception as e:
+                print(e)
+                break
+
+        listen_socket.close()
+
+    except KeyboardInterrupt:
+        print("\n Vou encerrar!")
+        pass
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
